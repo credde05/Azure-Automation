47619ddbbac3c055d2bfc5cbd1727507518fc640
Ubuntu <azureuser@pr5-vm.ueckqek0b03udpwxzmugb2deqh.ux.internal.cloudapp.net>
2025-04-18 03:18:17 +0000

    Add script to create VM

---
+++ Automation-Vm.sh
@@ -0,0 +1,8 @@
+az vm create \
+  --resource-group lucky\
+  --name TestingVm \
+  --image Ubuntu2404 \
+  --admin-username azureuser \
+  --authentication-type password \
+  --admin-password 'YourSecurePassword123!'
+:
